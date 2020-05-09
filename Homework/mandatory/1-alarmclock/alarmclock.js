e6023b7d8b24d08404067fcd88ad603e0635a6c0
changodiez <joaquindiezw@gmail.com>
2020-05-09 16:31:23 +0200

    AlarmClockTest

--- Homework/mandatory/1-alarmclock/alarmclock.js
+++ Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,5 +1,26 @@
-function setAlarm() {}
+function setAlarm() {
+  setTime = document.getElementById("alarmSet").value
+  timeRemaining = document.getElementById("timeRemaining");
+  timeRemaining.innerText = "Time Remaining: " + setTime;
+  setInterval(countdown, 1000);
+  countdown()
+}
 
+
+
+
+function countdown() {
+  if (setTime > 0) {
+    setTime--;
+    timeRemaining.innerHTML = "Time Remaining: " + setTime;
+  } else if (setTime === 0) {
+    playAlarm();
+    document.body.style.backgroundColor = "red";
+    setTime = null;
+    timeRemaining.innerHTML = "Time Remaining: 00:00";
+  }
+  
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
@@ -20,6 +41,7 @@ function playAlarm() {
 
 function pauseAlarm() {
   audio.pause();
+  document.body.style.backgroundColor = "white";
 }
 
 window.onload = setup;
